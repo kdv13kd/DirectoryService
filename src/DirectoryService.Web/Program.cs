67ffcddb010dde3a91e4b137b30e2ac11c4d5466
Konkoff Daniel <kdv13kd@mail.ru>
2025-09-30 20:35:05 +0300

    projects and folders created; swagger added (ds-b-2)

---
+++ src/DirectoryService.Web/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DirectoryService.Web;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "DirectoryService"));
+        }
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
